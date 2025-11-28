5329d84f72a0f440fa9303a2ebd8f1673ca05329
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-28 14:59:00 -0800

    Feat: Track cumulative total with shop item
    -Shop items add new features
    -Clicks are currencies that can redeem shop items
    -Redeeming a shop item will deduct from the current amount of clicks
    but will still keep count of total cumulative amount of clicks

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 const clickThisBtn = document.querySelector(".clickThisBtn");
-const pointTotal = document.querySelector(".pointTotal")
+const pointCurrentTotal = document.querySelector(".pointCurrentTotal")
+const pointCumulativeTotal = document.querySelector(".pointCumulativeTotal")
+const funnyText = document.querySelector(".funnyText")
+const shopColorChangeBtn = document.querySelector(".shopColorChangeBtn")
 let i = 0
-
+let p = 0
 clickThisBtn.addEventListener("click", () => {    
     i++;
-    pointTotal.textContent = `what the flip ${i}`})
+    p++;
+    pointCurrentTotal.textContent = `Current Total: ${i}`;
+    pointCumulativeTotal.textContent =`Cumulative Total: ${p}`;
+    if (i <=15) {funnyText.textContent = "what the flip... how could u have done this to me....."}
+    else {funnyText.textContent = "are u happy with yourself...";}})
+
+shopColorChangeBtn.addEventListener("click", () => {if (i == 10) {i = (i - 10);
+    pointCurrentTotal.textContent = `Current Total: ${i}`;}
+    clickThisBtn.style.cssText = "color: blue; background: cyan;";
+    shopColorChangeBtn.remove()}
+)
